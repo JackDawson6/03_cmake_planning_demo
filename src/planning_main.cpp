fc85c0345d6a866379dfae75bca888e1c35ef6b9
Jack Dawson <JackDawson2022@163.com>
2025-07-03 10:09:19 +0800

    basic mode

---
+++ src/planning_main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "process.h"
+using std::cout, std::endl;
+
+int main(){
+    cout << "planing start" << endl;
+    Process pro;
+    pro.planProcess();
+    cout << "planing end" << endl;
+}
---
+++ src/process/process.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "pnc_map.h"
+
+class Process
+{
+public:
+    void planProcess();
+
+public:
+    PNC_Map my_map;
+};
+
+

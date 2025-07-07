7b67e7e5609581e64fa33ce14980656ce42f45ef
Jack Dawson <JackDawson2022@163.com>
2025-07-07 09:18:01 +0800

    version 0.0.1

--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -6,5 +6,11 @@ int main(){
     cout << "planing start" << endl;
     Process pro;
     pro.planProcess();
+#ifdef DEBUG
+    cout << ">>>------------Debug mode is ON------------<<<" << endl;
+#else  
+    cout << ">>>------------Debug mode is OFF------------<<<" << endl;
+#endif
+
     cout << "planing end" << endl;
 }
